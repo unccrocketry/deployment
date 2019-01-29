ba22107f8cac145621fb7da5064cd480a13b58ae
unccrocketry <45529186+unccrocketry@users.noreply.github.com>
2019-01-28 23:41:47 -0500

    Update orientation.py

--- orientation/orientation.py
+++ orientation/orientation.py
@@ -19,36 +19,33 @@ CW = -90                            # Value for Clockwise Servo Rotation
 CCW = 90                            # Value for Counterclockwise Servo Rotation
 Stop = -10                          # Value to Stop Servo Rotation
 thresh_xacc = 22                    # Value of the x-axis accelerometer reading once airframe is considered to be in the proper orientation.
+keySet = 0                          # Validation bit checking to see if message to initialize deployment has been received by the board
+
 
 # Orientation Function. Accepts no arguments.
 # Required Global Objects/Definitions: {uart, orientServo, accel, CW, CCW, Stop}
 def orientation():                  
-    completed=0
-    uart.write('Stopping Servo')
+    completed=0                     #Validation bit to check if orientation is complete.
+    uart.write('Setting servo speed(s) to 0') 
     orientServo.speed(Stop)
-    uart.write('Waiting for Signal')
-
-    keySet = 0
+    uart.write('Awaiting for Signal')
     while keySet == 0:
          daInput = str(uart.read())
          if 'ok' in daInput:
             keySet = 1
 
     while completed==0:
-        uart.write('Starting shit')
-        uart.write('not yet completed')
+        uart.write('Starting Calibration')
         xacc= accel.x()
         yacc = accel.y()
         zacc = accel.z()
-        uart.write(str(xacc))
 
-        yf = float(yacc)
-        xf = float(xacc)
-        #ratio = yf/xf
-        STATEX = 1
-        state_x = math.copysign(STATEX,xacc)
+        # Obtain the polarities of each accelerometer axis reading (Mapping to unit circle quadrants)
+        STATEX = 1   
         STATEY = 1
-        state_y = math.copysign(STATEY,yacc)
+        
+        state_x = math.copysign(STATEX,xacc)    #Returns (-1/+1) for (-x/+x) accelerometer readings
+        state_y = math.copysign(STATEY,yacc)    #Returns (-1/+1) for (-y/+y) accelerometer readings
 
         if state_x > 0:
             if state_y > 0:
